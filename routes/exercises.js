5f3f408c834a5c4f88b69e8250425da497daca3a
AaliyahM777 <liyah.34544@gmail.com>
2019-10-20 21:49:15 -0400

    new update

--- routes/exercises.js
+++ routes/exercises.js
@@ -15,6 +15,14 @@ let Exercise = require('../models/exercise.model');
 // 1. get all exercise logs on record
 // GET: /
 // ========================================
+
+// telling our router including express use the get method of our home page endpoint
+// using async keyword = using asynchinous processing so we are going to use a promise
+// taking in request and response parameters and then we use try/ catch statement
+// we try block to create a exception, we try await our Exercise(exercise-model.js component) with find method
+// then we respond and send back our exercise
+// if our try isnt true it goes to our next exception catch
+//in catch we want a response back giving us a 500 status code error with a message
 router.get('/', async (req, res) => {
     try {
         const exercise = await Exercise.find()
@@ -29,6 +37,14 @@ router.get('/', async (req, res) => {
 // 2. add a new exercise log
 // POST: /add
 // ========================================
+
+
+// telling our router including express use the post method with our add endpoint
+// running in async format
+// within function we want create a new object that allows the user to create a new excerise log 
+// we set username,description, duration, and date and the req.body(inputs) as key value pairs. whatever user types in for each category will equal that
+//then save our excercise document using the .save method with an await callback and set it to document excercise
+// then we want a response to send our doc exercise (let excercise (var)
 router.post('/add', async (req, res) => {
     let exercise = new Exercise({
         username: req.body.username,
@@ -44,6 +60,11 @@ router.post('/add', async (req, res) => {
 // 3. retrieve a specfic exercise log
 // GET: /:id
 // ========================================
+
+//telling our router including express use the get method to find specific id with our find/:id  endpoint
+// same as above we use try and catch
+// try= find id from excercise model
+//catch= if error is caught send json response with status code number 500
 router.get('/find/:id', async (req, res) => {
     try {
         const exercise = await Exercise.findById(req.params.id)
@@ -59,6 +80,12 @@ router.get('/find/:id', async (req, res) => {
 // 4. delete a specfic exercise log
 // DELETE: /:id
 // ========================================
+
+//telling our router including express use the delete method to find specific id with our /:id  endpoint
+// same as above we use try and catch
+// try= find and delete id from excercise model
+//catch= if error is caught send json response with status code number 500
+
 router.delete('/:id', async (req, res) => {
     try {
         const exercise = await Exercise.findByIdAndDelete(req.params.id)
@@ -74,6 +101,13 @@ router.delete('/:id', async (req, res) => {
 // with information sent by client on req body
 // POST: /update/:id
 // ========================================
+
+//telling our router including express use the postmethod to update specific id with our/update /:id  endpoint
+// we use the update one method to update one specific id set to an array of objects
+// _id property is setting to req params id
+//and then we use $set property set to a new set of inputs including (username, duration, etc)
+// then we want to get a response sent back sneding excercise which is our new updated data
+
 router.post('/update/:id', async (req, res) => {
     let exercise = await Exercise.updateOne(
         {
@@ -93,5 +127,5 @@ router.post('/update/:id', async (req, res) => {
     res.send(exercise)
 })
 
-
+// we export router so we can import this file into other files 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,14 @@ let User = require('../models/user.model');
 // 1. get all users on record
 // GET: /
 // ========================================
+
+// telling our router including express use the get method of our home page endpoint
+// using async keyword = using asynchinous processing so we are going to use a promise
+// taking in request and response parameters and then we use try/ catch statement
+// we try block to create a exception, we try await our User(create-user.js component) with find method
+// then we respond and send back our user
+// if our try isnt true it goes to our next exception catch
+//in catch we want a response back giving us a 500 status code error with a message
 router.get('/', async (req,res)=>{
     try{
         const user = await User.find()
@@ -29,6 +37,15 @@ router.get('/', async (req,res)=>{
 // 2. add a new user
 // POST /add
 // ========================================
+
+// telling our router including express use the post method with our add endpoint
+// running in async format
+// within function we want create a new object that allows the user to create a new user with username
+// we set username and the req.body(input) as key value pair. whatever user types in for username will equal that
+//then save our user document using the .save method with an await callback and set it to document user
+// then we want a response to send our doc user (let user (var)
+
+
 router.post('/add', async (req, res) => {
     let user = new User({
         username: req.body.username
@@ -38,5 +55,5 @@ router.post('/add', async (req, res) => {
 
 })
 
-
+// we export router so we can import this file into other files 
 module.exports = router;
